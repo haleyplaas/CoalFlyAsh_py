8d87dfc4679d8c3dae1e5fbcb03cf7827c374ebb
Haley Plaas <hplaas@live.unc.edu>
2024-12-18 17:19:48 -0500

    12-18-2024

--- SSP370_input_datafiles.py
+++ SSP370_input_datafiles.py
@@ -1,192 +1,3 @@
-#%% DONT RE-RUN THIS REGRIDDER
-#from netCDF4 import Dataset
-
-# Quick view of some of the details within the netcdf file
-# Open the dataset 
-#dataset = Dataset('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN.nc', 'r')
-
-# List all variables and attributes
-#print(dataset.variables.keys())
-#print(dataset.variables['BC_em_anthro'])
-#print(dataset.variables['sector'])
-#print(dataset.variables['sector_bnds'])
-
-# 0: Agriculture; 1: Energy;  2: Industrial; 3: Transportation; 4: Residential, Commercial, Other; 5: Solvents production and application; 6: Waste; 7: International Shipping
-
-# FCOAL = 1 + 2 (FF) + 4 (BF)
-# FOIL = 3 (only from terrestrial cells) + 7 (only from ocean cells)
-# FWOOD = 4 (BF?)
-# FSMELT = bring over from Fe_Emissions_Fuel
-# I think I actually need to split coal into the following though, based on model variable pointers
-# FCOALFF = 1 + 2
-# FCOALBF = 4
-
-# F370[i,j,s] = BC370[i,j,s] / BCPD[i,j,s] * FePD[i,j,s]
-# where i is lon, j is lat, and s is sector 
-# calculate for each sector, and then add to create new consolidated variables
-# need to double check with Douglas on wood and FCOAL
-
-# visualizing the BC_emiss data for each sector in separate pandas dataframes
-#import xarray as xr
-#import pandas as pd
-#import numpy as np
-
-# Load the NetCDF file -- for Future simulation
-#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN.nc"
-#ds = xr.open_dataset(file_path)
-
-# grid dimensions are currently 360 lat x 720 lon
-# re-gridding to 192 lat x 288 lon
-#import xesmf as xe
-
-# Create target grid (192 x 288)
-#lat_target = xr.DataArray(
-    #data=np.linspace(-90, 90, 192),
-   # dims='lat',
-    #name='lat')
-#lon_target = xr.DataArray(
-  #  data=np.linspace(0, 360, 288, endpoint=False),  # 0 to 360 for 288 points
-   # dims='lon',
-    #name='lon')
-#target_grid = xr.Dataset({'lat': lat_target, 'lon': lon_target})
-
-# Apply regridding
-#regridder = xe.Regridder(ds, target_grid, method='bilinear')
-#ds_regridded = regridder(ds)
-
-# Save to a new NetCDF file
-#ds_regridded.to_netcdf('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN_regridded_288x192.nc') 
-
-# now for SSP370 File
-#from netCDF4 import Dataset
-# Open the dataset 
-#dataset = Dataset('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090-2100MEAN.nc', 'r')
-
-# List all variables and attributes
-#print(dataset.variables.keys())
-#print(dataset.variables['BC_em_anthro'])
-#print(dataset.variables['sector'])
-#print(dataset.variables['sector_bnds'])
-
-# the time variables needs to be condensed into a mean first -- time is 120 options, and need to regrid
-#import xarray as xr
-
-# Load the NetCDF file -- for Future simulation
-#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090_2100MEAN.nc"
-#ds = xr.open_dataset(file_path)
-#BC_em_anthro = ds['BC_em_anthro']
-
-# Calculate the mean across the `time` dimension
-#mean_BC_em_anthro = BC_em_anthro.mean(dim="time", keep_attrs=True)
-
-#mean_BC_em_anthro.to_netcdf("C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_MEAN.nc")
-
-# ----------------------------------------------------------------------------------------------------------------
-# grid dimensions are currently 360 lat x 720 lon
-# re-gridding to 192 lat x 288 lon
-#import xarray as xr
-#import xesmf as xe
-#import numpy as np
-
-#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090_2100MEAN.nc"
-#ds = xr.open_dataset(file_path)
-
-
-# Create target grid (192 x 288)
-#lat_target = xr.DataArray(
-   # data=np.linspace(-90, 90, 192),
-   # dims='lat',
-    #name='lat')
-#lon_target = xr.DataArray(
-   # data=np.linspace(0, 360, 288, endpoint=False),  # 0 to 360 for 288 points
-   ## dims='lon',
-   # name='lon')
-#target_grid = xr.Dataset({'lat': lat_target, 'lon': lon_target})
-
-# Apply regridding
-#regridder = xe.Regridder(ds, target_grid, method='bilinear')
-#ds_regridded = regridder(ds)
-
-# Save to a new NetCDF file
-#ds_regridded.to_netcdf('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090-2100MEAN_regridded_288x192.nc')
-
-# this whole regridder didnt work, 
-# instead it is necessary to use cdo remapbil 
-
-# %% DATAFRAMES FOR QUICK NUMBERS VIZ IN TABLE --------------------------------------------------------------------------
-# visualizing the BC_emiss data for each sector in separate pandas dataframes
-import xarray as xr
-import pandas as pd
-import numpy as np
-
-# Load the NetCDF file -- using newly regridded data
-file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN_regridded_288x192.nc"
-ds = xr.open_dataset(file_path)
-
-# Extract Black Carbon (BC) emission values SSP370 
-BC_em_anthro = ds['BC_em_anthro']  # Emission data
-lon = ds['lon'].values           # Longitude values -- the .values extracts as a numPy array automatically, removes metadata
-lat = ds['lat'].values            # Latitude values
-sector = ds['sector'].values      # Sector identifiers
-
-# Check
-print("lon", lon)
-print("lat", lat)
-print("sector", sector) # looks right 
-
-# dataframes are nice for quick viz but not great for manipulation and plotting, xarray is best 
-# Create a dictionary to store dataframes for each sector
-sector_dataframes = {}
-
-# For loop to loop through individual sectors
-for sec_idx, sec_value in enumerate(sector):
-    # Filter data for the current sector index
-    filtered_data = BC_em_anthro.isel(sector=sec_idx).squeeze()
-    
-    # Convert the data to a pandas DataFrame
-    df = pd.DataFrame(filtered_data.values, index=lat, columns=lon)
-    df.index.name = 'Latitude'
-    df.columns.name = 'Longitude'
-    
-    # Store the DataFrame in the dictionary
-    sector_dataframes[sec_value] = df
-
-# Access the dataframe for a specific sector, e.g., sector at midpoint `sector_value`
-BC_emiss_0 = sector[0]  # Adjust as needed
-BC_emiss_0_df = sector_dataframes[BC_emiss_0]
-# BC_emiss_0_df # pop out to jupyter notebook 
-
-# Sector 1: Energy
-BC_emiss_1 = sector[1]  
-BC_emiss_1_df = sector_dataframes[BC_emiss_1]
-# Sector 2: Industrial Coal
-BC_emiss_2 = sector[2] 
-BC_emiss_2_df = sector_dataframes[BC_emiss_2]
-# Sector 3: Transportation
-BC_emiss_3 = sector[3]  
-BC_emiss_3_df = sector_dataframes[BC_emiss_3]
-# Sector 4: Residential Coal
-BC_emiss_4 = sector[4]  # Adjust as needed
-BC_emiss_4_df = sector_dataframes[BC_emiss_4]
-# Sector 5: Solvents, etc. 
-BC_emiss_5 = sector[5]  # Adjust as needed
-BC_emiss_5_df = sector_dataframes[BC_emiss_5]
-# Sector 6: Waste
-BC_emiss_6 = sector[6]  # Adjust as needed
-BC_emiss_6_df = sector_dataframes[BC_emiss_6]
-# Sector 7: International Shipping
-BC_emiss_7 = sector[7]  # Adjust as needed
-BC_emiss_7_df = sector_dataframes[BC_emiss_7]
-
-# used this to do a quick check to make sure different values were coming up uniquely for each sector
-from itables import show
-# Show the DataFrame interactively
-show(BC_emiss_0_df, scrollY=True, scrollX=True, maxRows=10, maxColumns=200) # looks good, data is dif between sectors
-print(lon.shape, "lon dim")
-print(lat.shape, "lat values")
-
-
-
 #%% CHECKING TO SEE IF REGRIDDER WORKED PROPERLY USING CDO REMAPCON --------------------------------------------------
 # PASSED FOR PD FILES -- budgets add to the same value
 import numpy as np
@@ -1285,7 +1096,195 @@ print(quick_total_ratio_FU)
 # remapcon better for emissions, see differences between bilineation and conservative methods
 # ------------------------------------------------------------------------------------------------------------------------
 # %%
+#%% DONT RE-RUN THIS REGRIDDER
+#from netCDF4 import Dataset
+
+# Quick view of some of the details within the netcdf file
+# Open the dataset 
+#dataset = Dataset('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN.nc', 'r')
+
+# List all variables and attributes
+#print(dataset.variables.keys())
+#print(dataset.variables['BC_em_anthro'])
+#print(dataset.variables['sector'])
+#print(dataset.variables['sector_bnds'])
+
+# 0: Agriculture; 1: Energy;  2: Industrial; 3: Transportation; 4: Residential, Commercial, Other; 5: Solvents production and application; 6: Waste; 7: International Shipping
+
+# FCOAL = 1 + 2 (FF) + 4 (BF)
+# FOIL = 3 (only from terrestrial cells) + 7 (only from ocean cells)
+# FWOOD = 4 (BF?)
+# FSMELT = bring over from Fe_Emissions_Fuel
+# I think I actually need to split coal into the following though, based on model variable pointers
+# FCOALFF = 1 + 2
+# FCOALBF = 4
+
+# F370[i,j,s] = BC370[i,j,s] / BCPD[i,j,s] * FePD[i,j,s]
+# where i is lon, j is lat, and s is sector 
+# calculate for each sector, and then add to create new consolidated variables
+# need to double check with Douglas on wood and FCOAL
+
+# visualizing the BC_emiss data for each sector in separate pandas dataframes
+#import xarray as xr
+#import pandas as pd
+#import numpy as np
+
+# Load the NetCDF file -- for Future simulation
+#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN.nc"
+#ds = xr.open_dataset(file_path)
+
+# grid dimensions are currently 360 lat x 720 lon
+# re-gridding to 192 lat x 288 lon
+#import xesmf as xe
+
+# Create target grid (192 x 288)
+#lat_target = xr.DataArray(
+    #data=np.linspace(-90, 90, 192),
+   # dims='lat',
+    #name='lat')
+#lon_target = xr.DataArray(
+  #  data=np.linspace(0, 360, 288, endpoint=False),  # 0 to 360 for 288 points
+   # dims='lon',
+    #name='lon')
+#target_grid = xr.Dataset({'lat': lat_target, 'lon': lon_target})
+
+# Apply regridding
+#regridder = xe.Regridder(ds, target_grid, method='bilinear')
+#ds_regridded = regridder(ds)
+
+# Save to a new NetCDF file
+#ds_regridded.to_netcdf('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN_regridded_288x192.nc') 
+
+# now for SSP370 File
+#from netCDF4 import Dataset
+# Open the dataset 
+#dataset = Dataset('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090-2100MEAN.nc', 'r')
+
+# List all variables and attributes
+#print(dataset.variables.keys())
+#print(dataset.variables['BC_em_anthro'])
+#print(dataset.variables['sector'])
+#print(dataset.variables['sector_bnds'])
+
+# the time variables needs to be condensed into a mean first -- time is 120 options, and need to regrid
+#import xarray as xr
+
+# Load the NetCDF file -- for Future simulation
+#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090_2100MEAN.nc"
+#ds = xr.open_dataset(file_path)
+#BC_em_anthro = ds['BC_em_anthro']
+
+# Calculate the mean across the `time` dimension
+#mean_BC_em_anthro = BC_em_anthro.mean(dim="time", keep_attrs=True)
+
+#mean_BC_em_anthro.to_netcdf("C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_MEAN.nc")
+
+# ----------------------------------------------------------------------------------------------------------------
+# grid dimensions are currently 360 lat x 720 lon
+# re-gridding to 192 lat x 288 lon
+#import xarray as xr
+#import xesmf as xe
+#import numpy as np
+
+#file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090_2100MEAN.nc"
+#ds = xr.open_dataset(file_path)
+
+
+# Create target grid (192 x 288)
+#lat_target = xr.DataArray(
+   # data=np.linspace(-90, 90, 192),
+   # dims='lat',
+    #name='lat')
+#lon_target = xr.DataArray(
+   # data=np.linspace(0, 360, 288, endpoint=False),  # 0 to 360 for 288 points
+   ## dims='lon',
+   # name='lon')
+#target_grid = xr.Dataset({'lat': lat_target, 'lon': lon_target})
+
+# Apply regridding
+#regridder = xe.Regridder(ds, target_grid, method='bilinear')
+#ds_regridded = regridder(ds)
+
+# Save to a new NetCDF file
+#ds_regridded.to_netcdf('C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_ScenarioMIP_IAMC-AIM-ssp370-1-1_gn_2090-2100MEAN_regridded_288x192.nc')
+
+# this whole regridder didnt work, 
+# instead it is necessary to use cdo remapbil 
+
+# %% DATAFRAMES FOR QUICK NUMBERS VIZ IN TABLE --------------------------------------------------------------------------
+# visualizing the BC_emiss data for each sector in separate pandas dataframes
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+# Load the NetCDF file -- using newly regridded data
+file_path = "C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\BC-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_2000-2015MEAN_regridded_288x192.nc"
+ds = xr.open_dataset(file_path)
+
+# Extract Black Carbon (BC) emission values SSP370 
+BC_em_anthro = ds['BC_em_anthro']  # Emission data
+lon = ds['lon'].values           # Longitude values -- the .values extracts as a numPy array automatically, removes metadata
+lat = ds['lat'].values            # Latitude values
+sector = ds['sector'].values      # Sector identifiers
+
+# Check
+print("lon", lon)
+print("lat", lat)
+print("sector", sector) # looks right 
+
+# dataframes are nice for quick viz but not great for manipulation and plotting, xarray is best 
+# Create a dictionary to store dataframes for each sector
+sector_dataframes = {}
+
+# For loop to loop through individual sectors
+for sec_idx, sec_value in enumerate(sector):
+    # Filter data for the current sector index
+    filtered_data = BC_em_anthro.isel(sector=sec_idx).squeeze()
+    
+    # Convert the data to a pandas DataFrame
+    df = pd.DataFrame(filtered_data.values, index=lat, columns=lon)
+    df.index.name = 'Latitude'
+    df.columns.name = 'Longitude'
+    
+    # Store the DataFrame in the dictionary
+    sector_dataframes[sec_value] = df
+
+# Access the dataframe for a specific sector, e.g., sector at midpoint `sector_value`
+BC_emiss_0 = sector[0]  # Adjust as needed
+BC_emiss_0_df = sector_dataframes[BC_emiss_0]
+# BC_emiss_0_df # pop out to jupyter notebook 
+
+# Sector 1: Energy
+BC_emiss_1 = sector[1]  
+BC_emiss_1_df = sector_dataframes[BC_emiss_1]
+# Sector 2: Industrial Coal
+BC_emiss_2 = sector[2] 
+BC_emiss_2_df = sector_dataframes[BC_emiss_2]
+# Sector 3: Transportation
+BC_emiss_3 = sector[3]  
+BC_emiss_3_df = sector_dataframes[BC_emiss_3]
+# Sector 4: Residential Coal
+BC_emiss_4 = sector[4]  # Adjust as needed
+BC_emiss_4_df = sector_dataframes[BC_emiss_4]
+# Sector 5: Solvents, etc. 
+BC_emiss_5 = sector[5]  # Adjust as needed
+BC_emiss_5_df = sector_dataframes[BC_emiss_5]
+# Sector 6: Waste
+BC_emiss_6 = sector[6]  # Adjust as needed
+BC_emiss_6_df = sector_dataframes[BC_emiss_6]
+# Sector 7: International Shipping
+BC_emiss_7 = sector[7]  # Adjust as needed
+BC_emiss_7_df = sector_dataframes[BC_emiss_7]
+
+# used this to do a quick check to make sure different values were coming up uniquely for each sector
+from itables import show
+# Show the DataFrame interactively
+show(BC_emiss_0_df, scrollY=True, scrollX=True, maxRows=10, maxColumns=200) # looks good, data is dif between sectors
+print(lon.shape, "lon dim")
+print(lat.shape, "lat values")
 
 
 
-#ds_file.to_netcdf("C:\\Users\\heplaas\\OneDrive - North Carolina State University\\Collaborations\\Coal Fly Ash\\data\\RATIO_TEST.nc")
+
+
+
